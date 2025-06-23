2f193b41029e63624a59c1a7c8a2b471829b40f9
oyboy <atom_box687@mail.ru>
2025-06-23 00:20:17 -0700

    Second test-case

--- api-tests/src/test/java/org/example/api/AdminPageTest.java
+++ api-tests/src/test/java/org/example/api/AdminPageTest.java
@@ -1,51 +1,49 @@
 package org.example.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.example.api.config.ConfigReader;
 import org.example.api.config.CookieExtractor;
-import org.example.api.model.response.StaticTranslationsResponse;
+import org.example.api.model.request.GenericRequest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AdminPageTest {
     private static Map<String, String> cookie;
+    private static ObjectMapper objectMapper;
 
     @BeforeAll
     public static void setUp() {
         cookie = CookieExtractor.getCookie();
+        objectMapper = new ObjectMapper();
     }
 
     @Test
     public void getLanguagesList(){
-        String culture = "fr-fr";
-        int timezoneOffset = -180;
-        int langId = 8;
-        String skinName = "betsonic";
-        int configId = 1;
-        String countryCode = "";
-        String deviceType = "Mobile";
-        String numformat = "en";
-        String integration = "skintest";
+        GenericRequest request = GenericRequest.builder()
+                .timezoneOffset(-180)
+                .langId(8)
+                .skinName("betsonic")
+                .configId(1)
+                .culture("fr-fr")
+                .countryCode("")
+                .deviceType("Desktop")
+                .numformat("en")
+                .integration("skintest")
+                .build();
 
         Response response = given()
                 .log().all()
                 .baseUri(ConfigReader.getProperty("front_page"))
                 .cookies(cookie)
-                .queryParams("timezoneOffset", timezoneOffset,
-                        "langId", langId,
-                        "skinName", skinName,
-                        "configId", configId,
-                        "culture", culture,
-                        "countryCode", countryCode,
-                        "deviceType", deviceType,
-                        "numformat", numformat,
-                        "integration", integration)
+                .queryParams(objectMapper.convertValue(request, Map.class))
                 .when()
                 .get("/api/Translation/StaticTranslations")
                 .then()
@@ -53,13 +51,87 @@ public class AdminPageTest {
                 .extract()
                 .response();
 
-        StaticTranslationsResponse translationsResponse = response.as(StaticTranslationsResponse.class);
-
-        Map<String, String> res = translationsResponse.getResult();
+        JsonPath jsonPath = response.jsonPath();
+        Map<String, String> res = jsonPath.get("Result");
 
         assertFalse(res.isEmpty(), "Translation list should not be empty");
         assertEquals("Aujourd’hui", res.get("Today"), "Translation for 'Today' is incorrect");
         assertEquals("Mise", res.get("Stake"), "Translation for 'Stake' is incorrect");
         assertEquals("Paris introuvables", res.get("No bets found"), "Translation for 'No bets found' is incorrect");
+    }
+
+    @Test
+    public void checkViewAllEventsButtonIsNotDisplayed(){
+        GenericRequest request = GenericRequest.builder()
+                .timezoneOffset(420)
+                .langId(39)
+                .skinName("betsonic")
+                .configId(1)
+                .culture("fr-fr")
+                .countryCode("RU")
+                .deviceType("Desktop")
+                .numformat("en")
+                .integration("skintest")
+                .sportId(74)
+                .showAllEvents(false)
+                .count(10)
+                .hasStreaming(false)
+                .build();
+
+        Response response = given()
+                .log().all()
+                .baseUri(ConfigReader.getProperty("front_page"))
+                .cookies(cookie)
+                .queryParams(objectMapper.convertValue(request, Map.class))
+                .when()
+                .get("/api/Sportsbook/GetUpcoming")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath jsonPath = response.jsonPath();
+        boolean isDisplayed = jsonPath.getBoolean("Result.ShowMoreEvents");
+        int eventsCount = jsonPath.getInt("Result.EventsCount");
+
+        assertThat("Количество событий должно быть меньше 10", eventsCount < 10);
+        assertFalse(isDisplayed, "Кнопка показа всех событий не должна отображаться");
+    }
+    @Test
+    public void checkViewAllEventsButtonIsDisplayed(){
+        GenericRequest request = GenericRequest.builder()
+                .timezoneOffset(420)
+                .langId(39)
+                .skinName("betsonic")
+                .configId(1)
+                .culture("fr-fr")
+                .countryCode("RU")
+                .deviceType("Desktop")
+                .numformat("en")
+                .integration("skintest")
+                .sportId(76)
+                .showAllEvents(false)
+                .count(10)
+                .hasStreaming(false)
+                .build();
+
+        Response response = given()
+                .log().all()
+                .baseUri(ConfigReader.getProperty("front_page"))
+                .cookies(cookie)
+                .queryParams(objectMapper.convertValue(request, Map.class))
+                .when()
+                .get("/api/Sportsbook/GetUpcoming")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath jsonPath = response.jsonPath();
+        boolean isDisplayed = jsonPath.getBoolean("Result.ShowMoreEvents");
+        int eventsCount = jsonPath.getInt("Result.EventsCount");
+
+        assertThat("Количество событий должно быть больше или равно 10", eventsCount >= 10);
+        assertTrue(isDisplayed, "Кнопка показа всех событий должна отображаться");
     }
 }
---
+++ api-tests/src/test/java/org/example/api/model/request/GenericRequest.java
@@ -0,0 +1,56 @@
+package org.example.api.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GenericRequest {
+    @JsonProperty("timezoneOffset")
+    private Integer timezoneOffset;
+
+    @JsonProperty("langId")
+    private Integer langId;
+
+    @JsonProperty("skinName")
+    private String skinName;
+
+    @JsonProperty("configId")
+    private Integer configId;
+
+    @JsonProperty("culture")
+    private String culture;
+
+    @JsonProperty("countryCode")
+    private String countryCode;
+
+    @JsonProperty("deviceType")
+    private String deviceType;
+
+    @JsonProperty("numformat")
+    private String numformat;
+
+    @JsonProperty("integration")
+    private String integration;
+
+    @JsonProperty("sportId")
+    private Integer sportId;
+
+    @JsonProperty("showAllEvents")
+    private Boolean showAllEvents;
+
+    @JsonProperty("count")
+    private Integer count;
+
+    @JsonProperty("hasStreaming")
+    private Boolean hasStreaming;
+}
