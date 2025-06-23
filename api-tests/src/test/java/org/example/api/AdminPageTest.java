f89f822a9e778e113de50c3b981c0252878c20e8
oyboy <atom_box687@mail.ru>
2025-06-22 21:10:46 -0700

    First test-case

--- api-tests/src/test/java/org/example/api/AdminPageTest.java
+++ api-tests/src/test/java/org/example/api/AdminPageTest.java
@@ -1,17 +1,17 @@
 package org.example.api;
 
+import io.restassured.response.Response;
 import org.example.api.config.ConfigReader;
 import org.example.api.config.CookieExtractor;
-import org.example.api.model.response.Language;
-import org.example.api.model.response.LanguagesListResponse;
+import org.example.api.model.response.StaticTranslationsResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AdminPageTest {
     private static Map<String, String> cookie;
@@ -23,21 +23,43 @@ public class AdminPageTest {
 
     @Test
     public void getLanguagesList(){
-        LanguagesListResponse apiResponse = given()
-                .baseUri(ConfigReader.getProperty("base_page"))
+        String culture = "fr-fr";
+        int timezoneOffset = -180;
+        int langId = 8;
+        String skinName = "betsonic";
+        int configId = 1;
+        String countryCode = "";
+        String deviceType = "Mobile";
+        String numformat = "en";
+        String integration = "skintest";
+
+        Response response = given()
+                .log().all()
+                .baseUri(ConfigReader.getProperty("front_page"))
                 .cookies(cookie)
+                .queryParams("timezoneOffset", timezoneOffset,
+                        "langId", langId,
+                        "skinName", skinName,
+                        "configId", configId,
+                        "culture", culture,
+                        "countryCode", countryCode,
+                        "deviceType", deviceType,
+                        "numformat", numformat,
+                        "integration", integration)
                 .when()
-                .get("/Api/HighlightsManager/LanguagesList")
+                .get("/api/Translation/StaticTranslations")
                 .then()
                 .statusCode(200)
                 .extract()
-                .response()
-                .as(LanguagesListResponse.class);
+                .response();
 
-        List<Language> languages = apiResponse.getData();
-        /*languages.forEach(lang ->
-                System.out.println(lang.getName() + " (" + lang.getCode() + ")"));*/
+        StaticTranslationsResponse translationsResponse = response.as(StaticTranslationsResponse.class);
 
-        assertFalse(languages.isEmpty(), "Languages list should not be empty");
+        Map<String, String> res = translationsResponse.getResult();
+
+        assertFalse(res.isEmpty(), "Translation list should not be empty");
+        assertEquals("Aujourd’hui", res.get("Today"), "Translation for 'Today' is incorrect");
+        assertEquals("Mise", res.get("Stake"), "Translation for 'Stake' is incorrect");
+        assertEquals("Paris introuvables", res.get("No bets found"), "Translation for 'No bets found' is incorrect");
     }
 }
